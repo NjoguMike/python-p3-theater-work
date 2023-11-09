5bcd73194d38d86a0913653ac25f998568a2d093
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-09 13:25:31 +0300

    Seed Data added

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,28 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+    id = Column(Integer(), primary_key=True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(String())
+    role_id = Column(Integer(), ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'Audition({self.actor}, {self.location}, {self.hired})'
+        
+    
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer(), primary_key=True)
+    character_name = Column(String())
+
+    auditions = relationship('Audition', backref='role')
+
+    def __repr__(self):
+        return f'Role({self.character_name})'
---
+++ lib/seed.py
@@ -0,0 +1,42 @@
+from models import Base, Audition, Role
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from faker import Faker
+import random
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///theater.db', echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    fake = Faker()
+
+    session.query(Role).delete()
+    positions = ['co-lead', 'driver', 'fiction',
+        'lead-actor', 'cre-member', 'writer']
+    roles = []
+    for i in range(10):
+        role = Role(
+            character_name = random.choice(positions)
+        )
+
+        
+        session.add(role)
+        session.commit()
+        roles.append(role)
+    
+    session.query(Audition).delete()
+    auditions = []
+    for role in roles:
+        audition = Audition(
+            actor = fake.name(),
+            location = fake.unique.address(),
+            phone = fake.phone_number(),
+            hired = 0,
+            role_id = role.id
+        )
+
+        
+        session.add(audition)
+        session.commit()
+        auditions.append(audition)
